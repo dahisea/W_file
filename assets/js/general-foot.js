75d7dc24ce4f52a924b11a321d1c99dbb3b1960b
Dahi <81472122+dahisea@users.noreply.github.com>
2025-03-04 20:46:21 +0800

    更新 general-foot.js

--- assets/js/general-foot.js
+++ assets/js/general-foot.js
@@ -12,33 +12,6 @@ function calculateTime() {
 }
 setInterval(calculateTime, 1000);
 
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const divs = document.querySelectorAll('div');
-
-    divs.forEach(div => {
-        let currentStyle = div.getAttribute('style');
-        if (currentStyle) {
-            // 使用正则表达式匹配背景图片的 URL
-            let regex = /background-image:\s*url\(["']?(.*?)["']?\)/;
-            let match = regex.exec(currentStyle);
-
-            if (match) {
-                let currentUrl = match[1];
-                // 添加随机数参数
-                if (currentUrl.includes('?')) {
-                    currentUrl += `&random=${Math.random()}`;
-                } else {
-                    currentUrl += `?random=${Math.random()}`;
-                }
-                // 替换背景图片 URL，保留其他样式不变
-                let newStyle = currentStyle.replace(regex, `background-image: url('${currentUrl}')`);
-                div.setAttribute('style', newStyle);
-            }
-        }
-    });
-}); 
 async function fetchAPI() {
     try {
         const response = await fetch('https://capi.dahi.eu.org/whour');
@@ -53,26 +26,68 @@ async function fetchAPI() {
     }
 }
 
-function formatData(data) {
-    return `
-                <div class="data-section">
-                    <h3>IP 情報</h3>
-                    <p>
-                    都市: ${data.headers["cf-ipcity"]}<br>
-                    タイプ: ${data.headers["cf-ipcontinent"]}<br>
-                    国: ${data.country}<br>
-                    地域: ${data.headers["cf-region"]}<br>
-                    IP1: ${data.headers["x-real-ip"]}<br>
-                    IP2: ${data.headers["cf-connecting-ip"]}</p>
-                    <h3>その他情報</h3>
-                    <p>
-                    リクエスト元: ${data.headers["x-requested-with"]}<br>
-                    リファラー: ${data.referer}<br>
-                    受け入れ言語: ${data.acceptLanguage}<br>
-                    プラットフォーム: ${data.platform}<br>
-                    UTC 時間: ${data.utcTime}<br>
-                    ユーザーエージェント: ${JSON.stringify(data.userAgentInfo, null, 2)}</p>
-                </div>
-            `;
+function getLang() {
+    return document.documentElement.lang || 'en';
 }
+
+const translations = {
+    ja: {
+        ip_info: "IP 情報", request_info: "リクエスト情報", network_info: "ネットワーク情報", time_info: "時間情報",
+        city: "都市", continent: "大陸", country: "国", region: "地域", ip1: "IP1", ip2: "IP2",
+        request_from: "リクエスト元", referer: "リファラー", accept_language: "受け入れ言語", platform: "プラットフォーム",
+        utc_time: "UTC 時間", user_agent: "ユーザーエージェント"
+    },
+    zh-TW: {
+        ip_info: "IP 信息", request_info: "请求信息", network_info: "网络信息", time_info: "时间信息",
+        city: "城市", continent: "大洲", country: "国家", region: "地区", ip1: "IP1", ip2: "IP2",
+        request_from: "请求来源", referer: "来源", accept_language: "接受语言", platform: "平台",
+        utc_time: "UTC 时间", user_agent: "用户代理"
+    },
+    zh-CN: {
+        ip_info: "IP 信息", request_info: "请求信息", network_info: "网络信息", time_info: "时间信息",
+        city: "城市", continent: "大洲", country: "国家", region: "地区", ip1: "IP1", ip2: "IP2",
+        request_from: "请求来源", referer: "来源", accept_language: "接受语言", platform: "平台",
+        utc_time: "UTC 时间", user_agent: "用户代理"
+    },
+    en: {
+        ip_info: "IP Info", request_info: "Request Info", network_info: "Network Info", time_info: "Time Info",
+        city: "City", continent: "Continent", country: "Country", region: "Region", ip1: "IP1", ip2: "IP2",
+        request_from: "Requested From", referer: "Referer", accept_language: "Accept Language", platform: "Platform",
+        utc_time: "UTC Time", user_agent: "User-Agent"
+    }
+};
+
+function formatData(data) {
+    const lang = getLang();
+    const t = translations[lang];
+
+    return `
+        <div class="data-section">
+            <h3>${t.ip_info}</h3>
+            <p>
+            ${t.city}: ${data.headers?.["cf-ipcity"] || 'N/A'}<br>
+            ${t.continent}: ${data.headers?.["cf-ipcontinent"] || 'N/A'}<br>
+            ${t.country}: ${data.cfInfo?.country || 'N/A'}<br>
+            ${t.region}: ${data.headers?.["cf-region"] || 'N/A'}<br>
+            ${t.ip1}: ${data.headers?.["x-real-ip"] || 'N/A'}<br>
+            ${t.ip2}: ${data.headers?.["cf-connecting-ip"] || 'N/A'}</p>
+
+            <h3>${t.request_info}</h3>
+            <p>
+            ${t.request_from}: ${data.headers?.["x-requested-with"] || 'N/A'}<br>
+            ${t.referer}: ${data.headers?.referer || 'N/A'}<br>
+            ${t.accept_language}: ${data.headers?.["accept-language"] || 'N/A'}<br>
+            ${t.platform}: ${data.headers?.["sec-ch-ua-platform"] || 'N/A'}</p>
+
+            <h3>${t.time_info}</h3>
+            <p>
+            ${t.utc_time}: ${data.utcTime || 'N/A'}</p>
+
+            <h3>${t.network_info}</h3>
+            <p>
+            ${t.user_agent}: ${JSON.stringify(data.headers?.["user-agent"], null, 2) || 'N/A'}</p>
+        </div>
+    `;
+}
+
 fetchAPI();
